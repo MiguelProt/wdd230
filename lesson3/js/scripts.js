9ebdddd904b3fd7e9c4ccd07c76160ee986fe873
MiguelProt <prot.garcia@gmail.com>
2022-01-26 21:58:50 -0500

    updating css for responsive and improving the l&f

--- lesson3/js/scripts.js
+++ lesson3/js/scripts.js
@@ -11,3 +11,14 @@
     document.getElementById("year").innerHTML = year;
     document.getElementById("fullDate").innerHTML = dateTime;
  })();
+
+document.querySelector('.mobile-menu').addEventListener('click', (function(){
+    let menu = document.getElementById('sub-nav');
+    if ( menu.className.indexOf('show') > -1 ){
+        menu.classList.remove('show');
+        document.querySelector('.mobile-menu').textContent = "Show Menu";
+    } else {
+        menu.classList.add('show');
+        document.querySelector('.mobile-menu').textContent = "Hide Menu";
+    }
+}));
