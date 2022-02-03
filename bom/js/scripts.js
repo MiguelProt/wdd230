2d42a26569f5bb7a1d7bd36e1a8621babf5067d7
MiguelProt <prot.garcia@gmail.com>
2022-02-02 23:10:50 -0500

    uploading bom folder

---
+++ bom/js/scripts.js
@@ -0,0 +1,38 @@
+const input = document.getElementById("favchap");
+const button = document.querySelector("button[type='submit']");
+const list = document.querySelector(".list");
+
+button.setAttribute("disabled", true);
+
+button.addEventListener("click", function () {
+    console.log(input.value);
+    const createdLi = document.createElement('li');
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "X";
+    deleteButton.addEventListener("click", deleteItem);
+
+
+
+
+    createdLi.textContent = input.value;
+    createdLi.appendChild(deleteButton);
+    list.appendChild(createdLi);
+
+});
+
+function deleteItem() {
+    const parentLi = (this).parentNode.remove();
+
+    input.value = '';
+    input.focus();
+}
+
+input.addEventListener("input", function () {
+    console.log(input.value);
+    console.log(button);
+    if (input.value != '' && input.value != null )
+        button.removeAttribute("disabled");
+    else
+        button.setAttribute("disabled", true);
+});
