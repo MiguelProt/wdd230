f83930d3cc37dd92dc4813c085b4afc298cd3abd
MiguelProt <prot.garcia@gmail.com>
2022-02-26 22:07:00 -0500

    last_visited

--- chamber/js/localStorage.js
+++ chamber/js/localStorage.js
@@ -1,11 +1,30 @@
 const visits = document.querySelector(".visits");
-const currentVal = (window.localStorage.getItem('visits') == null ) ? 0 : parseInt(window.localStorage.getItem('visits'));
-console.log(currentVal);
+const date = document.querySelector(".date");
+
+const FACTOR = 1000 * 60 * 60 * 24;
+
+last_v = (window.localStorage.getItem('last_visit') == null) ? new Date().getTime() : window.localStorage.getItem('last_visit');
+new_v = new Date().getTime();
+
+let daysBetween = Math.round((new_v - last_v) / FACTOR)
+
+console.log(window.localStorage.getItem('visits'));
+console.log(daysBetween, ( new_v - last_v), 12);
+console.log(new_v);
+console.log(last_v);
+
 if (window.localStorage.getItem('visits') == null ) {
-    console.log('This is your first time in the date');
     window.localStorage.setItem('visits', 1);
 } else {
-    window.localStorage.setItem('visits', (currentVal + 1));
+    console.log('Mas de una vuelta');
+    window.localStorage.setItem('visits', parseInt(window.localStorage.getItem('visits')) + 1);
 }
 
-visits.textContent = 'Visits: ' + currentVal;
+console.log(window.localStorage.getItem('visits'), window.localStorage.getItem('last_visit'));
+
+days_message = (window.localStorage.getItem('last_visit') == null) ? ', Welcome by First Time!' : ', Last Visited: ' + daysBetween + ' day(s) ago.';
+
+visits.textContent = '# Visits: ' + window.localStorage.getItem('visits') + days_message;
+
+window.localStorage.setItem('last_visit', new_v);
+//localStorage.clear()
